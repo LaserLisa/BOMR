30dac1c46ead40e1585978d3dbdcfe41a9d4b2cc
Sara Anejjar <sara.anejjar@epfl.ch>
2024-12-04 20:56:56 +0100

    Add smoothing step in local nav

--- src/motion_control/local_navigation.py
+++ src/motion_control/local_navigation.py
@@ -1,59 +1,75 @@
 import asyncio
+import math
 
-obstThrL = 5          # low obstacle threshold to switch state local->global (equivalent to 2000)
-obstThrH = 8.5          # high obstacle threshold to switch state global->local (equivalent to 4000)
+# This code was mainly inspired by the homeworks of the Basics of Mobile Robotics at EPFL (Sessions 3 and 4)
+
+obstThrL = 5                # low obstacle threshold to switch state local->global
+obstThrH = 8.5              # high obstacle threshold to switch state global->local
 w_l = [10, 5, -5, -5, -10]  # weights for left motor
 w_r = [-10, -5, -5, 5, 10]  # weights for right motor
 nb_active_sensors = 0
+previous_direction = 'left'
 
 def get_navigation_state(driver, state):
     """
-    Update the navigation state based on the proximity sensor values.
+    Get the navigation state based on the proximity sensor values.
     Args:
-        obst (list): List of proximity sensor values.
         driver (Driving): Instance of the Driving class.
-        state (int): Current state of the navigation.
+        state (int): Current navigation state.
+                    state = 0 -> global navigation
+                    state = 1 -> local navigation
+                    state = 2 -> kidnapping
     Returns:
-        current_state (int): Updated state of the navigation: "global"=0 or "local"=1.
+        current_state (int): New navigation state.
     """
     global nb_active_sensors
     current_state = state
     obst = asyncio.run(driver.get_prox_horizontal())
     ground = asyncio.run(driver.get_prox_ground())
-    # state = 0 -> global navigation
-    # state = 1 -> local navigation
-    # state = 2 -> kidnapping
+
     if ground[0] == 0 or ground[1] == 0:
         current_state = 2
+
     elif state == 1:
         if all(sensor < obstThrL for sensor in obst):
             if nb_active_sensors != 0:
-                print("Entered loop to move")
-                driver.move(50 + (10 * nb_active_sensors))
+                # Move by the number of active sensors + the width of the robot
+                driver.move(60 + (10 * nb_active_sensors))
+            
+                # Smoothing step to avoid repeated state switching
+                if previous_direction == 'left':
+                    driver.turn(math.pi/4)
+                elif previous_direction == 'right':
+                    driver.turn(-math.pi/4)
+                driver.move(100) 
+
                 nb_active_sensors = 0
+
             current_state = 0
+
     elif state == 0: # global navigation
         if any(sensor > obstThrH for sensor in obst):
             nb_active_sensors = sum(sensor > 0 for sensor in obst)
-            print("nb of active sensors: ", nb_active_sensors)
             current_state = 1
+
     elif state == 2:
         current_state = 2
     return current_state, obst
 
-def calculate_new_motor_speed(obst):
+def calculate_new_motors_speed(obst):
     """
-    Calculate the new motor speeds based on the proximity sensor values and the weights for the left and right motor.
+    Calculate the new motor speeds based on the proximity sensor values.
+    It also updates the previous direction of the robot.
     Args:
-        obst (list): List of proximity sensor values.
-        w_l (list): List of weights for the left motor.
-        w_r (list): List of weights for the right motor.
+        obst (list): List of the first 5 proximity sensor values.
     Returns:
-        motor_left_target (int): New motor speed for the left motor.
-        motor_right_target (int): New motor speed for the right motor.
+        y (list): List of motor speeds for the left and right motor.
     """
+    global previous_direction
     y = [0, 0]
     for i in range(len(obst)):
-        y[0] = y[0] + w_l[i] * obst[i]
-        y[1] = y[1] + w_r[i] * obst[i]
+        y[0] = y[0] + w_l[i] * obst[i] # Left motor
+        y[1] = y[1] + w_r[i] * obst[i] # Right motor
+    previous_direction = 'right' if y[0] < y[1] else 'left'
     return y[0], y[1]
+
